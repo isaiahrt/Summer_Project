e091c5ca4bedf7303ada23436ff2bbfd164c2140
andriesfeder <afeder@wpa145018.wireless.mcgill.ca>
2017-05-16 12:19:46 -0400

    Team Constructor Edit
    removed addUser, getTaskList, Team and getTaskList methods

--- src/starter/Team.java
+++ src/starter/Team.java
@@ -3,36 +3,24 @@ package starter;
 import java.util.ArrayList;
 
 public class Team {
-	
-	private String aName;
-	private ArrayList<User> aUsers = new ArrayList<User>();
-	
-	public String getName() {
-		return aName;
-	}
-	
-	public void addUser(User pUser) {
-		aUsers.add(pUser);
-	}
-	
-	public Team(String pName) {
-		aName = pName;
-	}
-	
-	public void getTaskList() {
-		for(User u: aUsers) {
-			u.getTasks();
-		}
-	}
 
-	/**
-	 * constructor for team
-	 * @param aName: team name
-	 * @param aUsers: array list of users that make up the team
-	 */
-	public Team(String aName, ArrayList<User> aUsers) {
-		this.aName = aName;
-		this.aUsers = aUsers;
-	}
+
+  private String aName;
+  private ArrayList<User> aUsers = new ArrayList<User>();
+
+  public String getName() {
+    return aName;
+  }
+
+  	/**
+  	 * constructor for team
+  	 * @param aName: team name
+  	 * @param aUsers: array list of users that make up the team
+  	 */
+
+  	public Team(String aName, ArrayList<User> aUsers) {
+  		this.aName = aName;
+  		this.aUsers = aUsers;
+  	}
 
 }
